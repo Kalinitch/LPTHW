a3e52941e4b571609104fc738221acc0b66783b0
Kalinitch <31504128+Kalinitch@users.noreply.github.com>
2017-11-28 00:29:23 +0200

    4

--- exp.py
+++ exp.py
@@ -10,6 +10,7 @@ def game():
     print "So, wanna see a trick? yes or no? "
 
     choice = raw_input("> ")
+	
     if choice == "yes":
         print ">Would you believe me if I told you that I can guess your birthday?"
         if choice == "yes":
@@ -23,9 +24,11 @@ def game():
     elif choice == "no":
         print ">Ay, doesn't matter, I will do it anyways"
 
-print ">Would you believe me if I told you that I can guess your birthday?"
+def preparation():
+    print ">Would you believe me if I told you that I can guess your birthday?"
 
     choice2 = raw_input("> ")
+	
     if choice2 == "yno":
         print ">I see, no fun allowed here :( "
         print ("Bye " + yourname + ",what a loser!")
@@ -34,14 +37,14 @@ print ">Would you believe me if I told you that I can guess your birthday?"
         print ">Good, now you'll see the power of Homer 2000 in action."
     else:
         print ">Ay, doesn't matter, I will do it anyways"
-print ">Multiply the day of your birth by 2 (e.g. 12*2)"
-print ">Now, please add 5 to it"
-print ">For the next one you might want to grab a calculator"
-print ">Multiply the current result of your calculations by 50"
-print ">Add the number of the month of your birthday to the result of your calculations (e.g. January - 1; February - 2 etc.)"
-print ">Enter the result of your calculations"
+def trick ():
+    print ">Multiply the day of your birth by 2 (e.g. 12*2)"
+    print ">Now, please add 5 to it"
+    print ">For the next one you might want to grab a calculator"
+    print ">Multiply the current result of your calculations by 50"
+    print ">Add the number of the month of your birthday to the result of your calculations (e.g. January - 1; February - 2 etc.)"
+    print ">Enter the result of your calculations"
 
-    def trick ():
         int x, y
         x = raw_input("> ")
 
@@ -54,7 +57,9 @@ print "*thinking hard*"
 print (">Alakazam! Your birthday is on " + day + monthsname [month-1])
 
 print ">Are you impressed by my terrific might " + yourname + " ?"
+
       choice3 = raw_input("> ")
+	  
       for choice3 == "no":
           print ">Are you sure? Maybe you might want to reconsider? Take your time, I'll wait"
           print ">So, did you think about it? I'm going to ask you again"
